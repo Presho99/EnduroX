3450774e739a706ba169f37b597a200444773dab
Precious <Preshomondi@gmail.com>
2024-04-24 19:17:24 +0300

    Initial commit

---
+++ backend/routes/workout.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const Workout = require('../models/workoutModel')
+
+const router = express.Router()
+
+// GET all workouts
+router.get('/', (req, res) => {
+    res.json({message: 'GET all workouts'})
+
+})
+
+// GET  a single workout
+router.get('/:id', (req, res) => {
+    res.json({message: "GET a single workout"})
+})
+
+// POST a new workout
+router.post('/', async (req, res) => {
+    const {title, reps, load} = req.body
+
+    try {
+        const workout = await Workout.create({title, reps, load})
+        res.status(200).json(workout)
+
+    }catch(error){
+        res.status(400).json({error: error.message})
+
+    }
+   
+})
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+    res.json({message: 'DELETE a workout'})
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res) => {
+    res.json({message: 'EDIT a workout'})
+})
+
+module.exports = router
